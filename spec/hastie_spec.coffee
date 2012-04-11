22ab8ad39b7ed27745e50cf850835d144ede628e
Polarblau <polarblau@gmail.com>
2012-04-11 22:09:58 +0300

    Setup test env.

--- spec/hastie_spec.coffee
+++ spec/hastie_spec.coffee
@@ -1,6 +1,20 @@
 describe 'hastie', ->
 
-  #
+  $comments    = null
 
-  it 'should be truthy', ->
-    expect(true).toBeTruthy()
+  COMMENTS_URL = 'https://api.github.com/repos/rails/rails/commits/b1e55041ebb33a27121eff4424eeeaee4e4b5028/comments'
+
+  # SETUP
+
+  beforeEach ->
+    setFixtures "<div id='comments' data-comments-url='#{COMMENTS_URL}'></div>"
+    $comments = $('#comments').hastie()
+
+
+  # SPECS
+
+  describe 'basic jQuery plugin functionality', ->
+
+    it 'should be chainable', ->
+      $comments.addClass 'foo'
+      expect($comments).toHaveClass('foo')
--- spec/hastie_spec.js
+++ spec/hastie_spec.js
@@ -2,8 +2,18 @@
 (function() {
 
   describe('hastie', function() {
-    return it('should be truthy', function() {
-      return expect(true).toBeTruthy();
+    var $comments, COMMENTS_URL;
+    $comments = null;
+    COMMENTS_URL = 'https://api.github.com/repos/rails/rails/commits/b1e55041ebb33a27121eff4424eeeaee4e4b5028/comments';
+    beforeEach(function() {
+      setFixtures("<div id='comments' data-comments-url='" + COMMENTS_URL + "'></div>");
+      return $comments = $('#comments').hastie();
+    });
+    return describe('basic jQuery plugin functionality', function() {
+      return it('should be chainable', function() {
+        $comments.addClass('foo');
+        return expect($comments).toHaveClass('foo');
+      });
     });
   });
 
